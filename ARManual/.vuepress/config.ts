23ed11506732fa00a5167311c86e80f9e1a905f5
domnantas <domantas@zyro.com>
2021-08-22 19:05:13 +0300

    Add missing curly brace

--- ARManual/.vuepress/config.ts
+++ ARManual/.vuepress/config.ts
@@ -85,6 +85,7 @@ export default {
       {
         property: 'viewport',
         content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+      }
     ]
   ],
   themeConfig: {
